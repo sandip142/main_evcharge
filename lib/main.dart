be79dc389e7798faf23de677ac589d0f7f62c4f7
sandip kankhare <sandipkankhare940@gmail.com>
2024-10-21 20:28:04 +0530

    add form and payment gateway

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:main_evcharge/Screen/Splace/splace_screen.dart';
 import 'package:main_evcharge/Screen/timer_flutter.dart';
 import 'package:main_evcharge/firebase_options.dart';
 
